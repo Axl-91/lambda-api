2b783a8b472c2110bb373b76d24681bd7cc6dfbb
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2026-08-18 02:19:21 -0300

    Init

---
+++ sam-app/hello-world/app.ts
@@ -0,0 +1,41 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'hello world',
+                method: event.httpMethod,
+                event: event.path,
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
+
+export const createLoyaltyCard = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        return {
+            statusCode: 201,
+            body: JSON.stringify({
+                message: 'Created',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
