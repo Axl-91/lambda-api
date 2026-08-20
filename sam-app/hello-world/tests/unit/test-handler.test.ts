e74096e31f4d678ebf6af69c4fe9dfd29779978b
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2026-08-20 12:20:11 -0300

    Refactors

--- sam-app/hello-world/tests/unit/test-handler.test.ts
+++ sam-app/hello-world/tests/unit/test-handler.test.ts
@@ -1,64 +1,39 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { lambdaHandler } from '../../app';
+import { createLoyaltyCard, helloWorldLambda } from '../../app';
 import { expect, describe, it } from '@jest/globals';
 
-describe('Unit test for app handler', function () {
-    it('verifies successful response', async () => {
-        const event: APIGatewayProxyEvent = {
-            httpMethod: 'get',
-            body: '',
-            headers: {},
-            isBase64Encoded: false,
-            multiValueHeaders: {},
-            multiValueQueryStringParameters: {},
-            path: '/hello',
-            pathParameters: {},
-            queryStringParameters: {},
-            requestContext: {
-                accountId: '123456789012',
-                apiId: '1234',
-                authorizer: {},
-                httpMethod: 'get',
-                identity: {
-                    accessKey: '',
-                    accountId: '',
-                    apiKey: '',
-                    apiKeyId: '',
-                    caller: '',
-                    clientCert: {
-                        clientCertPem: '',
-                        issuerDN: '',
-                        serialNumber: '',
-                        subjectDN: '',
-                        validity: { notAfter: '', notBefore: '' },
-                    },
-                    cognitoAuthenticationProvider: '',
-                    cognitoAuthenticationType: '',
-                    cognitoIdentityId: '',
-                    cognitoIdentityPoolId: '',
-                    principalOrgId: '',
-                    sourceIp: '',
-                    user: '',
-                    userAgent: '',
-                    userArn: '',
-                },
-                path: '/hello',
-                protocol: 'HTTP/1.1',
-                requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
-                requestTimeEpoch: 1428582896000,
-                resourceId: '123456',
-                resourcePath: '/hello',
-                stage: 'dev',
-            },
-            resource: '',
-            stageVariables: {},
-        };
-        const result: APIGatewayProxyResult = await lambdaHandler(event);
+describe('Hello world function', () => {
+    it('GET /hello with valid data', async () => {
+        const event = {
+            httpMethod: 'GET',
+        } as APIGatewayProxyEvent;
+
+        const result: APIGatewayProxyResult = await helloWorldLambda(event);
 
         expect(result.statusCode).toEqual(200);
         expect(result.body).toEqual(
             JSON.stringify({
                 message: 'hello world',
+            }),
+        );
+    });
+});
+
+describe('Create loyalty card', () => {
+    it('creates a loyalty card with valid data', async () => {
+        const event = {
+            httpMethod: 'POST',
+            body: JSON.stringify({
+                name: 'Axel',
+            }),
+        } as APIGatewayProxyEvent;
+
+        const result: APIGatewayProxyResult = await createLoyaltyCard(event);
+
+        expect(result.statusCode).toEqual(201);
+        expect(result.body).toEqual(
+            JSON.stringify({
+                message: 'Created',
             }),
         );
     });
